54b175150cabeff7c007e0956a42874190198492
Paul Borza <paul@borza.ro>
2017-02-24 18:41:45 -0800

    should work

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2017 Paul Borza
+ *
+ * This software may be modified and distributed under the terms
+ * of the MIT license. See the LICENSE file for details.
+ */
+
+import { P2cBalancer } from './P2cBalancer';
+import { RandomBalancer } from './RandomBalancer';
+
+export {
+    P2cBalancer,
+    RandomBalancer,
+};
