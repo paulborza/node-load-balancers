71b471b146cdc0a322432774f5fe79996994f4b4
Paul Borza <paul@borza.ro>
2017-02-26 20:15:07 -0800

    better looking chart

--- docs/plot_errors.py
+++ docs/plot_errors.py
@@ -4,33 +4,40 @@
 # pip install matplotlib numpy
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import os
 
 bar_width = 0.35
 random_balancer = (0.2016, 0.2008, 0.2017, 0.1943, 0.2016)
 p2c_balancer = (0.2, 0.2001, 0.2, 0.1999, 0.2)
 
+# IMPORTANT: matplotlib won't find latex otherwise.
+os.environ['PATH'] = os.environ['PATH'] + ':/Library/TeX/texbin/'
+
 # plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
-plt.subplots()
+
+fig, ax = plt.subplots()
+ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda y, _: '{:.0%}'.format(y)))
 
 bins = np.arange(5)
 
 plt.bar(bins, random_balancer, bar_width, alpha=0.8,
-    color='b', label='Random Balancer')
+    color='orangered', label='Random Balancer')
  
 plt.bar(bins + bar_width, p2c_balancer, bar_width, alpha=0.8,
-    color='g', label='The Power of 2 Choices (P2c) Balancer')
+    color='limegreen', label='The Power of 2 Choices (P2c) Balancer')
 
 plt.axhline(y=0.2, linestyle='dashed',
-    color='g', label='Ideal load balancer')
+    color='green', label=r'Ideal load balancer')
 
-plt.title('Comparison of load balancers')
+plt.title('Comparison of load balancers', fontsize=20, y=1.02)
 
 plt.ylim([0.19, 0.21])
-plt.ylabel(r'traffic percentage')
+plt.ylabel('Traffic Percentage')
 
 plt.xticks(bins + bar_width, ('1', '2', '3', '4', '5'))
-plt.xlabel(r'proxies')
+plt.xlabel('Proxies')
 
 plt.legend()
 plt.savefig('errors.png')
