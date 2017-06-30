fc3fbdeecb49f46021a353ca57d536ade1678d99
Paul Borza <paul@borza.ro>
2017-06-30 14:22:20 -0700

    Update plot.py

--- docs/plot.py
+++ docs/plot.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+#
+# Copyright (c) 2017-present Paul Borza
+#
+# This software may be modified and distributed under the terms
+# of the MIT license. See the LICENSE file for details.
+#
 
 # brew cask install mactex
 # pip install matplotlib numpy
