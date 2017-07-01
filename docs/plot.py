78ede001856a20091c953754883184e64c6010f3
Paul Borza <paul@borza.ro>
2017-06-30 17:05:56 -0700

    proper x axis

--- docs/plot.py
+++ docs/plot.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import os
 
-bar_width = 0.35
+bar_width = 0.3
 random_balancer = (99716, 100226, 100051, 99987, 100161, 99496, 100045, 99965, 100211, 100142)
 p2c_balancer = (100001, 99999, 100000, 100000, 100000, 99999, 100002, 99999, 100000, 100000)
 
@@ -35,7 +35,7 @@ bins = np.arange(10)
 
 plt.ylim([99000, 101000])
 
-ax1.bar(bins, random_balancer, bar_width, alpha=0.8,
+ax1.bar(bins + bar_width, random_balancer, bar_width, alpha=0.8,
     color='orangered')
 ax2.bar(bins + bar_width, p2c_balancer, bar_width, alpha=0.8,
     color='limegreen')
@@ -43,18 +43,19 @@ ax2.bar(bins + bar_width, p2c_balancer, bar_width, alpha=0.8,
 ax1.set_title('Random load balancer', fontsize=14)
 ax2.set_title('Power of two choices load balancing algorithm', fontsize=14)
 
-for ax in [ax1, ax2]:
-    ax.axhline(y=100000, linestyle='dashed',
-        color='green', label='Ideal load balancer')
-
-plt.xticks(bins + bar_width, ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10'))
-
 ax2.yaxis.tick_right()
 ax2.yaxis.set_label_position('right')
 
 for ax in [ax1, ax2]:
+    ax.axhline(y=100000, linestyle='dashed',
+        color='green', label='Ideal load balancer')
+
+    ax.set_xticks(bins + 1.5 * bar_width)
+    ax.set_xticklabels(range(1, 11))
+
     ax.set_ylabel('Requests')
     ax.set_xlabel('Proxies')
+
     ax.legend(frameon=False)
 
 plt.savefig('comparison.png', dpi=400)
